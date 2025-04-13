da15d8e4ae3316138c50a1691a393039bac6706c
CodeEx222 <fanghaibin_222@hotmail.com>
2025-04-13 19:33:09 +0800

    动作调整提交

--- client/FightDemo/Source/FightDemo/fight/FightComponent.cpp
+++ client/FightDemo/Source/FightDemo/fight/FightComponent.cpp
@@ -16,7 +16,7 @@ UFightComponent::UFightComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	PlayerActionStateBitset = std::make_shared<std::bitset<32>>();
 }
 
 
@@ -55,9 +55,11 @@ void UFightComponent::BeginPlay()
 		return;
 	}
 
-	canInputRecord = true;
-	canPlayAttack = true;
-	isInCombo = false;
+
+	// 可以攻击
+	SetPlayerActionState(EPlayerState::CanAttack);
+	// 可以记录输入
+	SetPlayerActionState(EPlayerState::CanRecordInput);
 }
 
 
@@ -96,11 +98,11 @@ void UFightComponent::AddInput(EInputEnum inputEnum)
 	case EInputEnum::HeavyAttack:
 	case EInputEnum::Defend:
 		// 判断当前人物状态是否可以接受指令
-		if ( canInputRecord)
+		if ( GetPlayerActionState(EPlayerState::CanRecordInput))
 		{
-			InputArray.Add( {inputEnum, GameTime, true} );
+			AddNewInput( {inputEnum, GameTime, true} );
 			// 假设当前人物没有在攻击状态的话,就可以检测出招
-			if (canPlayAttack)
+			if ( GetPlayerActionState(EPlayerState::CanAttack))
 			{
 				CheckAttack();
 			}
@@ -113,72 +115,7 @@ void UFightComponent::AddInput(EInputEnum inputEnum)
 }
 
 
-
-FAttackAnimTable* UFightComponent::CheckInput()
-{
-	//const double GameTime = UGameplayStatics::GetTimeSeconds(this);
-
-	// 检测 InputArray 最后一个是否为新的输入,否则不检测
-	if (InputArray.Num() == 0 || !InputArray[InputArray.Num()-1].IsNewCheck )
-	{
-		return nullptr;
-	}
-
-	// 循环所有的出招表,看是否有符合出招的招式
-	for (int i = 0; i < AttackAnimTableArray.Num(); i++)
-	{
-		auto AnimTable = AttackAnimTableArray[i];
-		bool bIsfind = true;
-
-		if (AnimTable->MoveInputs.Num() > 0 && MoveInputArray.Num() > 0)
-		{
-			for (int j = 0; j < AnimTable->MoveInputs.Num(); j++)
-			{
-				auto checkEnum = AnimTable->MoveInputs[j];
-				TArray<EInputEnum> checkArray;
-				for (auto Element : MoveInputArray)
-				{
-					checkArray.Add(Element.InputEnum);
-				}
-
-				if (!checkArray.Contains(checkEnum))
-				{
-					bIsfind = false;
-					break;
-				}
-			}
-		}
-
-		if (bIsfind)
-		{
-			// 检测 input是否一致
-			if (AnimTable->Inputs.Num() == InputArray.Num())
-			{
-				// 完全匹配
-				for (int j = 0; j < AnimTable->Inputs.Num(); ++j)
-				{
-					if (AnimTable->Inputs[j] != InputArray[j].InputEnum)
-					{
-						bIsfind = false;
-						break;
-					}
-				}
-			}
-		}
-		if (bIsfind)
-		{
-			// 找到匹配的动作
-			return AnimTable;
-		}
-
-	}
-
-	return nullptr;
-}
-
-
-
-void UFightComponent::CheckActionTimeLine(int64 CurrentTime, float DeltaTime)
+void UFightComponent::CheckActionTimeLine(int64 CurrentTime, float DeltaTime) const
 {
 	// 获取当前角色
 	//auto OwnCharacterObj = Cast<AGameFightCharacter>(GetOwner());
@@ -234,18 +171,52 @@ void UFightComponent::PlaySkill(FAttackAnimTable* SkillToPlay)
 	const auto anim = SkillToPlay->ActionAnimMontage;
 	const auto PlayMontage = anim.LoadSynchronous();
 
-	canPlayAttack = false;
-	AnimInstance->Montage_Play(PlayMontage);
+	AnimInstance->PlayFightMontage(PlayMontage,1,0,FName("Start"), true);
+
+	// 循环动画的所有通知, 注册到时间线当中
+	// for (int i = 0; i < PlayMontage->Notifies.Num(); i++)
+	// {
+	// 	auto notify = PlayMontage->Notifies[i];
+	// 	if (notify.Notify != nullptr)
+	// 	{
+	// 		auto notifyState = Cast<UFightAnimNotify>(notify.Notify);
+	// 		if (notifyState != nullptr)
+	// 		{
+	// 			auto notifyTime = notify.GetTime();
+	// 			auto notifyEnum = notifyState->AnimEnum;
+	//
+	// 			auto skillActionInfo = FightTimeLineObj->GetFreeObj(true);
+	// 			skillActionInfo->NodeId = NodeIdGenFactory::GenId();
+	// 			skillActionInfo->ActionTime = notifyTime;
+	// 			skillActionInfo->ActionLineState = EActionLineState::SkillStart;
+	// 			FightTimeLineObj->Add(skillActionInfo);
+	// 		}
+	// 	}
+	// 	else if (notify.NotifyStateClass != nullptr)
+	// 	{
+	// 		auto notifyState = Cast<UFightAnimNotifyState>(notify.NotifyStateClass);
+	// 		if (notifyState != nullptr)
+	// 		{
+	// 			auto notifyTime = notify.GetTime();
+	// 			auto notifyEnum = notifyState->AnimEnum;
+	//
+	// 			auto skillActionInfo = FightTimeLineObj->GetFreeObj(true);
+	// 			skillActionInfo->NodeId = NodeIdGenFactory::GenId();
+	// 			skillActionInfo->ActionTime = notifyTime;
+	// 			skillActionInfo->ActionLineState = EActionLineState::SkillStart;
+	// 			FightTimeLineObj->Add(skillActionInfo);
+	// 		}
+	// 	}
+	//
+	// }
 
 }
 
 void UFightComponent::CheckAttack()
 {
-	auto skillToPlay = CheckInput();
-	if (skillToPlay == nullptr)
+	const auto SkillToPlay = CheckInput();
+	if (SkillToPlay == nullptr)
 	{
-		// 没有找到符合的动作,那么连击指示器置为false
-		isInCombo = false;
 		return;
 	}
 
@@ -256,8 +227,18 @@ void UFightComponent::CheckAttack()
 	}
 
 	//把接受输入置为false
-	canInputRecord = false;
-	PlaySkill(skillToPlay);
+	SetPlayerActionState(EPlayerState::CanRecordInput,false);
+
+	// 检测当前是否在播放战斗动作
+	if (GetPlayerActionState(EPlayerState::InPlayAttack))
+	{
+		// 如果在播放战斗动作,那么就把当前的动作结束, 进入连击状态
+		SetPlayerActionState(EPlayerState::InComboNext);
+	}
+	SetPlayerActionState(EPlayerState::InPlayAttack);
+
+	SetPlayerActionState(EPlayerState::CanAttack,false);
+	PlaySkill(SkillToPlay);
 }
 
 void UFightComponent::OnAnimNotify(UAnimNotify * Notify)
@@ -278,8 +259,8 @@ void UFightComponent::OnAnimNotify(UAnimNotify * Notify)
 		}
 	case EFightAnimNotify::AttackNext:
 		{
-				isInCombo = true;
 				// 可以播放下次攻击
+				SetPlayerActionState(EPlayerState::CanAttack);
 				CheckAttack();
 		}
 	default: ;
@@ -302,18 +283,130 @@ void UFightComponent::OnAnimNotifyState(UAnimNotifyState * NotifyState, bool bSt
 	case EFightAnimStateNotify::None:
 		break;
 	case EFightAnimStateNotify::PlayState:
-			// 连击有效区间
-			clearInputRecord = !bStart;
+			SetPlayerActionState(EPlayerState::InPlayAttack,bStart);
+
+			if (bStart)
+			{
+				// 新的开始, 把连击置空
+				SetPlayerActionState(EPlayerState::InComboNext, false);
+			}
+			else
+			{
+				// 连击有效区间
+				if (GetPlayerActionState(EPlayerState::InComboNext))
+				{
+					// 是连击结束的,就不需要清空输入了
+				}
+				else
+				{
+					// 其他结束,清空输入
+					InputArray.Empty();
+				}
+			}
+
 		break;
 	case EFightAnimStateNotify::FanJiState:
 			// 反击区间
 		break;
 	case EFightAnimStateNotify::InoputState:
 			// 输入区间
-			canInputRecord = bStart;
-
+		SetPlayerActionState(EPlayerState::CanRecordInput, true);
 		break;
 	}
 
 
 }
+
+#pragma region 玩家输入检测
+
+void  UFightComponent::AddNewInput(const FInputElement& NewInput)
+{
+	// 拿取 InputArray 最后一个元素
+	if (InputArray.Num() > 0)
+	{
+		if (const auto LastInput = InputArray.Last(); LastInput.IsNewCheck)
+		{
+			// 如果最后一个元素是新的输入,那么就覆盖
+			InputArray.RemoveAt(InputArray.Num() - 1);
+		}
+	}
+	InputArray.Add(NewInput);
+}
+
+FAttackAnimTable* UFightComponent::CheckInput()
+{
+	//const double GameTime = UGameplayStatics::GetTimeSeconds(this);
+
+	// 检测 InputArray 最后一个是否为新的输入,否则不检测
+	if (InputArray.Num() == 0 || !InputArray[InputArray.Num()-1].IsNewCheck )
+	{
+		return nullptr;
+	}
+
+	// 循环所有的出招表,看是否有符合出招的招式
+	for (int i = 0; i < AttackAnimTableArray.Num(); i++)
+	{
+		auto AnimTable = AttackAnimTableArray[i];
+		bool bIsfind = false;
+
+		// if (AnimTable->MoveInputs.Num() > 0 && MoveInputArray.Num() > 0)
+		// {
+		// 	for (int j = 0; j < AnimTable->MoveInputs.Num(); j++)
+		// 	{
+		// 		auto checkEnum = AnimTable->MoveInputs[j];
+		// 		TArray<EInputEnum> checkArray;
+		// 		for (auto Element : MoveInputArray)
+		// 		{
+		// 			checkArray.Add(Element.InputEnum);
+		// 		}
+		//
+		// 		if (!checkArray.Contains(checkEnum))
+		// 		{
+		// 			bIsfind = false;
+		// 			break;
+		// 		}
+		// 	}
+		// }
+
+		// 检测 input是否一致
+		if (AnimTable->Inputs.Num() == InputArray.Num())
+		{
+			bIsfind = true;
+			// 完全匹配
+			for (int j = 0; j < AnimTable->Inputs.Num(); ++j)
+			{
+				if (AnimTable->Inputs[j] != InputArray[j].InputEnum)
+				{
+					bIsfind = false;
+					break;
+				}
+			}
+		}
+
+
+		if (bIsfind)
+		{
+			// 找到匹配的动作
+			return AnimTable;
+		}
+
+	}
+
+	return nullptr;
+}
+
+#pragma endregion
+
+#pragma region 玩家状态检测
+
+void UFightComponent::SetPlayerActionState(EPlayerState Type, const bool Value /* = true */) const
+{
+	PlayerActionStateBitset->set(static_cast<size_t>(Type), Value);
+}
+
+bool UFightComponent::GetPlayerActionState(EPlayerState Type) const
+{
+	return  PlayerActionStateBitset->test(static_cast<size_t>(Type));
+}
+
+#pragma endregion
---
+++ client/FightDemo/Source/FightDemo/ui/TimeLineDraw.h
@@ -0,0 +1,17 @@
+// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TimeLineDraw.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIGHTDEMO_API UTimeLineDraw : public UUserWidget
+{
+	GENERATED_BODY()
+
+};
